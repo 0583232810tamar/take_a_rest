21e72ea885ba1a2a534f88368d2e86dc1fbe156f
Tamar_GIT <t0583232810@gmail.com>
2026-07-14 13:28:10 +0300

    הוספת ENV לGITIGNOR

--- server/index.js
+++ server/index.js
@@ -149,7 +149,7 @@ io.on('connection', (socket) => {
 });
 
 // --- 🗄️ חיבור ל-MongoDB והרצת השרת ---
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/vacation_db')
     .then(() => {
         console.log('🚀 מחובר בהצלחה ל-MongoDB');
